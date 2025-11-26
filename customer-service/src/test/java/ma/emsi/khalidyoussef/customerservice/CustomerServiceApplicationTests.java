010e79016fd6dc1252c14d1ab25ec1ce7c0976a9
Ykhali <Youssef.Khalid@emsi-edu.ma>
2025-11-26 11:28:35 +0100

    2 eme commit: Add module customer-service : initial

---
+++ customer-service/src/test/java/ma/emsi/khalidyoussef/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.khalidyoussef.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

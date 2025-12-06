ac5e9169724fed792739ab146fdeb0987462718e
Ykhali <Youssef.Khalid@emsi-edu.ma>
2025-12-06 10:04:14 +0100

    11eme commit: activate eureka server in DiscoveryServiceApplication.java

--- discovery-service/src/main/java/com/example/khalidyoussef/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/example/khalidyoussef/discoveryservice/DiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.khalidyoussef.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // pour activer le serveur
 public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {

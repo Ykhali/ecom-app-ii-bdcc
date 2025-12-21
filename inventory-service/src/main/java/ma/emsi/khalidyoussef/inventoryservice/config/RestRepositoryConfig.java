f99b5eb8bed203f62dc3b705d5704a8b726e82b0
Ykhali <Youssef.Khalid@emsi-edu.ma>
2025-12-21 18:30:06 +0100

    Add annotation @configuration in inventoryservice/config/RestRepositoryConfig.java

--- inventory-service/src/main/java/ma/emsi/khalidyoussef/inventoryservice/config/RestRepositoryConfig.java
+++ inventory-service/src/main/java/ma/emsi/khalidyoussef/inventoryservice/config/RestRepositoryConfig.java
@@ -1,10 +1,12 @@
 package ma.emsi.khalidyoussef.inventoryservice.config;
 
 import ma.emsi.khalidyoussef.inventoryservice.entities.Product;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+@Configuration
 public class RestRepositoryConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {

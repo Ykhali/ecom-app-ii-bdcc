ad5712b1fd7dd08985d5fe620f3f974508fc401e
Ykhali <Youssef.Khalid@emsi-edu.ma>
2025-12-21 19:35:45 +0100

    Create component orders

--- ecom-web-app/src/app/app.module.ts
+++ ecom-web-app/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { provideHttpClient } from '@angular/common/http';
 import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    CustomersComponent
+    CustomersComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
--- ecom-web-app/src/app/customers/customers.component.ts
+++ ecom-web-app/src/app/customers/customers.component.ts
@@ -23,4 +23,7 @@ export class CustomersComponent implements OnInit{
     })
   }
 
+  getOrders(c: any) {
+    
+  }
 }

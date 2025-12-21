c14f552bbbc6036d6bb462329ddd64cd89b97240
Ykhali <Youssef.Khalid@emsi-edu.ma>
2025-12-21 17:37:31 +0100

    Add component  products and modify app.module.ts

--- ecom-web-app/src/app/app.module.ts
+++ ecom-web-app/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
